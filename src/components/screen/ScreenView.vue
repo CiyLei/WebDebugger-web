ba82f200f815e17b0463f2a32cf93fd9fb01c250
cl <903736662@qq.com>
2019-11-05 12:25:42 +0800

    opt: 在截屏/录屏界面进行Android5.1崩溃的提示

--- src/components/screen/ScreenView.vue
+++ src/components/screen/ScreenView.vue
@@ -1,22 +1,28 @@
 <template>
     <div>
+        <el-alert
+                title="警告：如果你是的手机版本是Android5.1（SDK：22）的话，在申请截屏/录屏权限的时候千万不要勾选“不再提示”，如果不慎勾选请在设置中关闭，实在不行请重装App，否则此功能会导致App奔溃。"
+                type="error"
+                center
+                show-icon>
+        </el-alert>
         <div style="margin-top: 80px">
             <ScrrenImageView v-for="item in mediaList" v-bind:url="item" v-bind:key="item"></ScrrenImageView>
         </div>
         <el-tooltip class="item" effect="dark" content="刷新" placement="bottom">
-            <el-button icon="el-icon-refresh" circle @click="getList" style="position: fixed; left: 20px; top: 80px"></el-button>
+            <el-button icon="el-icon-refresh" circle @click="getList" style="position: fixed; left: 20px; top: 120px"></el-button>
         </el-tooltip>
         <el-tooltip class="item" effect="dark" content="截屏" placement="bottom">
-            <el-button icon="el-icon-camera" circle @click="screenCapture" style="position: fixed; left: 70px; top: 80px"></el-button>
+            <el-button icon="el-icon-camera" circle @click="screenCapture" style="position: fixed; left: 70px; top: 120px"></el-button>
         </el-tooltip>
         <el-tooltip class="item" effect="dark" content="开始录屏" placement="bottom">
-            <el-button icon="el-icon-video-camera" circle @click="startScreenRecording" style="position: fixed; left: 130px; top: 80px"></el-button>
+            <el-button icon="el-icon-video-camera" circle @click="startScreenRecording" style="position: fixed; left: 130px; top: 120px"></el-button>
         </el-tooltip>
         <el-tooltip class="item" effect="dark" content="结束录屏" placement="bottom">
-            <el-button icon="el-icon-switch-button" circle @click="stopScreenRecording" style="position: fixed; left: 190px; top: 80px"></el-button>
+            <el-button icon="el-icon-switch-button" circle @click="stopScreenRecording" style="position: fixed; left: 190px; top: 120px"></el-button>
         </el-tooltip>
         <el-tooltip class="item" effect="dark" content="清除" placement="bottom">
-            <el-button icon="el-icon-delete" circle @click="clean" style="position: fixed; left: 250px; top: 80px"></el-button>
+            <el-button icon="el-icon-delete" circle @click="clean" style="position: fixed; left: 250px; top: 120px"></el-button>
         </el-tooltip>
         <div style="clear:both"></div>
         <footer style="height: 50px"></footer>
