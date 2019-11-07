648883ab3e2e89d8193c66a7ec3609a1aa61fe25
CiyLei <903736662@qq.com>
2019-11-07 21:30:59 +0800

    mod: 修改手机图片的绝对定位

--- src/components/adb/AdbView.vue
+++ src/components/adb/AdbView.vue
@@ -1,17 +1,17 @@
 <template>
     <el-container>
         <el-aside width="500px">
-            <div style="position: fixed; top: 120px; width: 500px">
+            <div style="position: absolute; top: 120px; width: 500px">
                 <el-image :src="require('../../assets/phone.webp')"></el-image>
             </div>
-            <div style="position: fixed; width: 60px; height: 60px; top: 800px; left: 100px;" @click="handleBack"></div>
-            <div style="position: fixed; width: 60px; height: 60px; top: 800px; left: 178px;" @click="handleMenu"></div>
-            <div style="position: fixed; width: 60px; height: 60px; top: 800px; left: 256px;" @click="handleSearch"></div>
-            <div style="position: fixed; width: 60px; height: 60px; top: 800px; left: 334px;" @click="handleHome"></div>
-            <div style="position: fixed; width: 30px; height: 65px; top: 320px; left: 40px;" @click="handleVolumePlus"></div>
-            <div style="position: fixed; width: 30px; height: 65px; top: 385px; left: 40px;" @click="handleVolumeLess"></div>
-            <div style="position: fixed; width: 30px; height: 80px; top: 250px; left: 435px; " @mousedown="handlePowerDown" @mouseup="handlePowerUp"></div>
-            <AdbScreenView style="position: fixed; background-color: rgb(51,153,102); left: 80px; top: 230px;"></AdbScreenView>
+            <div style="position: absolute; width: 60px; height: 60px; top: 800px; left: 100px;" @click="handleBack"></div>
+            <div style="position: absolute; width: 60px; height: 60px; top: 800px; left: 178px;" @click="handleMenu"></div>
+            <div style="position: absolute; width: 60px; height: 60px; top: 800px; left: 256px;" @click="handleSearch"></div>
+            <div style="position: absolute; width: 60px; height: 60px; top: 800px; left: 334px;" @click="handleHome"></div>
+            <div style="position: absolute; width: 30px; height: 65px; top: 320px; left: 40px;" @click="handleVolumePlus"></div>
+            <div style="position: absolute; width: 30px; height: 65px; top: 385px; left: 40px;" @click="handleVolumeLess"></div>
+            <div style="position: absolute; width: 30px; height: 80px; top: 250px; left: 435px; " @mousedown="handlePowerDown" @mouseup="handlePowerUp"></div>
+            <AdbScreenView style="position: absolute; background-color: rgb(51,153,102); left: 80px; top: 230px;"></AdbScreenView>
         </el-aside>
         <el-main>Main</el-main>
     </el-container>
